859980d864069f7c40ee67919a7ed28999143920
levensta <levensta@student.21-school.ru>
2021-03-16 23:41:43 +0300

    add strcmp, strcpy

---
+++ ft_strcpy.s
@@ -0,0 +1,22 @@
+global	_ft_strcpy
+
+section	.text
+_ft_strcpy:
+
+push rdi ; dst
+push rsi ; src
+mov rax, 0
+
+loop:
+cmp		BYTE [rsi + rax], 0
+jz return
+mov dl, BYTE [rsi + rax]
+mov BYTE [rdi + rax], dl
+inc rax
+jmp loop
+
+return:
+mov rax, rdi
+pop rdi
+pop rsi
+ret
--- main.c
+++ main.c
@@ -6,19 +6,35 @@
 /*   By: levensta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 05:25:34 by levensta          #+#    #+#             */
-/*   Updated: 2021/03/14 06:41:18 by levensta         ###   ########.fr       */
+/*   Updated: 2021/03/16 23:07:56 by levensta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
-int		ft_strlen(char const *str);
+// int		ft_strlen(char const *str);
+// char	*ft_strcpy(char *dst, char const *src);
+int		ft_strcmp(const char *s1, const char *s2);
 
 int main()
 {
-	int n = ft_strlen("123456789");
-	printf("%d\n", n);
+	// int n = ft_strlen("123456789");
+	// printf("%d\n", n);
+	
+	// char	*dst = malloc(10);
+	// char	*src = "123456789";
+	// printf("|%s|\n", strcpy(dst, src));
+	// printf("|%s|\n", dst);
+	// printf("|%s|\n", src);
+	// printf("==========\n");
+	// printf("|%s|\n", ft_strcpy(dst, src));
+	// printf("|%s|\n", dst);
+	// printf("|%s|\n", src);
+	
+	// printf("%d\n", strcmp("0123", 0));
+	// printf("%d\n", ft_strcmp("0123", 0));
 	return (0);
 }
 
